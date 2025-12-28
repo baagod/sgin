cebbc7b7b062e39d0979aa28910b9f5ef04ab279
baagod <baagod@proton.me>
2025-12-29 00:08:04 +0800

    feat：将 `ParamXX` 方法和上下文实现同步到 v1

--- ctx.go
+++ ctx.go
@@ -27,7 +27,7 @@ const (
 type Ctx struct {
 	Request *http.Request
 	Writer  gin.ResponseWriter
-	Params  gin.Params
+	Uris    gin.Params
 	Keys    map[string]any
 
 	engine  *Engine
@@ -43,7 +43,7 @@ func newCtx(ctx *gin.Context, e *Engine) *Ctx {
 		traceid: ctx.GetHeader(HeaderXRequestID),
 		Request: ctx.Request,
 		Writer:  ctx.Writer,
-		Params:  ctx.Params,
+		Uris:    ctx.Params,
 		Keys:    ctx.Keys,
 	}
 
@@ -57,8 +57,8 @@ func newCtx(ctx *gin.Context, e *Engine) *Ctx {
 
 // ------ 参数获取 ------
 
-// Values 获取所有参数 (Body 覆盖 Query)
-func (c *Ctx) Values() map[string]any {
+// Params 获取所有 请求 (含 Body) 参数
+func (c *Ctx) Params() map[string]any {
 	if c.cache != nil {
 		return c.cache
 	}
@@ -66,36 +66,40 @@ func (c *Ctx) Values() map[string]any {
 	c.cache = map[string]any{}
 	ct := c.GetHeader(HeaderContentType)
 
-	_ = c.Request.ParseForm()
-	for k, v := range c.Request.Form {
-		if len(v) > 0 {
-			c.cache[k] = v[0]
-		}
-	}
-
-	// 处理 JSON
 	if strings.HasPrefix(ct, MIMEJSON) {
 		r := bytes.NewReader(c.RawBody())
 		dec := sonic.ConfigDefault.NewDecoder(r)
 		dec.UseNumber()
 		_ = dec.Decode(&c.cache)
 	} else if strings.HasPrefix(ct, MIMEXML) || strings.HasPrefix(ct, MIMETextXML) {
-		// 处理 XML
 		if m, _ := mxj.NewMapXml(c.RawBody()); m != nil {
 			c.cache = m
 		}
-	} else if strings.HasPrefix(ct, MIMEMultipartForm) {
-		// 处理 Multipart Form
-		if form, err := c.ctx.MultipartForm(); err == nil {
-			for k, v := range form.Value {
-				if len(v) > 0 {
-					c.cache[k] = v[0]
-				}
+	}
+
+	partForm, err := c.ctx.MultipartForm()
+
+	for k, v := range c.Request.Form {
+		if vLen := len(v); vLen == 1 {
+			c.cache[k] = v[0]
+		} else if vLen > 1 {
+			c.cache[k] = v
+		}
+	}
+
+	if err == nil && strings.HasPrefix(ct, MIMEMultipartForm) {
+		for k, v := range partForm.Value {
+			if vLen := len(v); vLen == 1 {
+				c.cache[k] = v[0]
+			} else if vLen > 1 {
+				c.cache[k] = v
 			}
-			for k, v := range form.File {
-				if len(v) > 0 {
-					c.cache[k] = v[0] // *multipart.FileHeader
-				}
+		}
+		for k, v := range partForm.File {
+			if vLen := len(v); vLen == 1 {
+				c.cache[k] = v[0]
+			} else if vLen > 1 {
+				c.cache[k] = v
 			}
 		}
 	}
@@ -103,86 +107,94 @@ func (c *Ctx) Values() map[string]any {
 	return c.cache
 }
 
-// Value 获取请求参数
-func (c *Ctx) Value(key string, def ...any) string {
+// Param 获取请求参数
+func (c *Ctx) Param(key string, def ...any) string {
 	if len(def) == 0 {
-		return cast.ToString(c.ValueAny(key))
+		return cast.ToString(c.ParamAny(key))
 	}
-	return cast.ToString(c.ValueAny(key, def[0]))
+	return cast.ToString(c.ParamAny(key, def[0]))
 }
 
-// ValueAny 获取原始类型的参数值
-func (c *Ctx) ValueAny(key string, def ...any) any {
-	v, ok := c.Values()[key]
+// ParamAny 获取原始类型的参数值
+func (c *Ctx) ParamAny(key string, def ...any) any {
+	v, ok := c.Params()[key]
 	if !ok && len(def) > 0 {
 		return def[0]
 	}
 	return v
 }
 
-func (c *Ctx) ValueInt(key string, def ...any) int {
-	return cast.ToInt(c.ValueAny(key, def...))
+func (c *Ctx) ParamInt(key string, def ...any) int {
+	return cast.ToInt(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueInt8(key string, def ...any) int8 {
-	return cast.ToInt8(c.ValueAny(key, def...))
+func (c *Ctx) ParamInt8(key string, def ...any) int8 {
+	return cast.ToInt8(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueInt16(key string, def ...any) int16 {
-	return cast.ToInt16(c.ValueAny(key, def...))
+func (c *Ctx) ParamInt16(key string, def ...any) int16 {
+	return cast.ToInt16(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueInt32(key string, def ...any) int32 {
-	return cast.ToInt32(c.ValueAny(key, def...))
+func (c *Ctx) ParamInt32(key string, def ...any) int32 {
+	return cast.ToInt32(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueInt64(key string, def ...any) int64 {
-	return cast.ToInt64(c.ValueAny(key, def...))
+func (c *Ctx) ParamInt64(key string, def ...any) int64 {
+	return cast.ToInt64(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueUint(key string, def ...any) uint {
-	return cast.ToUint(c.ValueAny(key, def...))
+func (c *Ctx) ParamUint(key string, def ...any) uint {
+	return cast.ToUint(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueUint8(key string, def ...any) uint8 {
-	return cast.ToUint8(c.ValueAny(key, def...))
+func (c *Ctx) ParamUint8(key string, def ...any) uint8 {
+	return cast.ToUint8(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueUint16(key string, def ...any) uint16 {
-	return cast.ToUint16(c.ValueAny(key, def...))
+func (c *Ctx) ParamUint16(key string, def ...any) uint16 {
+	return cast.ToUint16(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueUint32(key string, def ...any) uint32 {
-	return cast.ToUint32(c.ValueAny(key, def...))
+func (c *Ctx) ParamUint32(key string, def ...any) uint32 {
+	return cast.ToUint32(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueUint64(key string, def ...any) uint64 {
-	return cast.ToUint64(c.ValueAny(key, def...))
+func (c *Ctx) ParamUint64(key string, def ...any) uint64 {
+	return cast.ToUint64(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueFloat32(key string, def ...any) float32 {
-	return cast.ToFloat32(c.ValueAny(key, def...))
+func (c *Ctx) ParamFloat32(key string, def ...any) float32 {
+	return cast.ToFloat32(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueFloat64(key string, def ...any) float64 {
-	return cast.ToFloat64(c.ValueAny(key, def...))
+func (c *Ctx) ParamFloat64(key string, def ...any) float64 {
+	return cast.ToFloat64(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueBool(key string, def ...any) bool {
-	return cast.ToBool(c.ValueAny(key, def...))
+func (c *Ctx) ParamBool(key string, def ...any) bool {
+	return cast.ToBool(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueTime(key string, def ...any) time.Time {
-	return cast.ToTime(c.ValueAny(key, def...))
+func (c *Ctx) ParamTime(key string, def ...any) time.Time {
+	return cast.ToTime(c.ParamAny(key, def...))
 }
 
-func (c *Ctx) ValueDuration(key string, def ...any) time.Duration {
-	return cast.ToDuration(c.ValueAny(key, def...))
+func (c *Ctx) ParamDuration(key string, def ...any) time.Duration {
+	return cast.ToDuration(c.ParamAny(key, def...))
 }
 
-// ValueFile 获取上传的文件
-func (c *Ctx) ValueFile(key string) (*multipart.FileHeader, error) {
+// ParamFile 获取上传的文件
+func (c *Ctx) ParamFile(key string) (*multipart.FileHeader, error) {
 	return c.ctx.FormFile(key)
+}
+
+func (c *Ctx) ParamArray(key string) []string {
+	return cast.ToStringSlice(c.ParamAny(key))
+}
+
+func (c *Ctx) ParamMap(key string) map[string]string {
+	return cast.ToStringMapString(c.ParamAny(key))
 }
 
 func (c *Ctx) SaveFile(file *multipart.FileHeader, dst string) error {
@@ -199,6 +211,10 @@ func (c *Ctx) IP() string {
 	return c.ctx.ClientIP()
 }
 
+func (c *Ctx) RemoteIP() string {
+	return c.ctx.RemoteIP()
+}
+
 // Path 返回请求路径
 // 假设请求: /users/123/profile?view=full&name=John%20Doe
 // Path() 返回: "/users/123/profile"
@@ -210,8 +226,13 @@ func (c *Ctx) Path(full ...bool) string {
 	return c.ctx.Request.URL.Path
 }
 
-func (c *Ctx) Param(key string) string {
-	return c.Params.ByName(key)
+func (c *Ctx) Uri(key string) string {
+	return c.ctx.Param(key)
+}
+
+func (c *Ctx) AddUri(key, value string) *Ctx {
+	c.ctx.AddParam(key, value)
+	return c
 }
 
 // GetHeader 获取 HTTP 请求头的值，如果不存在则返回可选的默认值。
@@ -240,8 +261,12 @@ func (c *Ctx) Cookie(name string) (string, error) {
 	return c.ctx.Cookie(name)
 }
 
-func (c *Ctx) SetCookie(name, value string, maxAge int, path, domain string, secure, httpOnly bool) {
+func (c *Ctx) SetCookie(name, value string, maxAge int, path, domain string, secure, httpOnly bool, samesite ...http.SameSite) *Ctx {
+	if len(samesite) > 0 {
+		c.ctx.SetSameSite(samesite[0])
+	}
 	c.ctx.SetCookie(name, value, maxAge, path, domain, secure, httpOnly)
+	return c
 }
 
 // ------ 响应控制 ------
@@ -287,19 +312,59 @@ func (c *Ctx) SendHTML(name string, data any) error {
 	return nil
 }
 
-func (c *Ctx) SendFile(path string) error {
+func (c *Ctx) SendBinary(data []byte) error {
 	c.ctx.Abort()
-	c.ctx.File(path)
+	c.ctx.Data(c.StatusCode(), c.GetHeader(HeaderContentType), data)
 	return nil
 }
 
-func (c *Ctx) SendDownload(file string, name ...string) error {
-	filename := filepath.Base(file)
-	if len(name) > 0 {
-		filename = name[0]
+// SendFile 发送文件，传递 name 为下载流。
+func (c *Ctx) SendFile(file string, name ...string) error {
+	if c.ctx.Abort(); len(name) > 0 {
+		filename := filepath.Base(file)
+		if name[0] != "" {
+			filename = name[0]
+		}
+		disposition := `attachment; filename*=UTF-8''` + url.QueryEscape(filename)
+		c.Header(HeaderContentDisposition, disposition)
 	}
-	disposition := `attachment; filename*=UTF-8''` + url.QueryEscape(filename)
-	return c.Header(HeaderContentDisposition, disposition).SendFile(file)
+	c.ctx.File(file)
+	return nil
+}
+
+// SendStream 发送流响应并返回布尔值，标识 “客户端是否在流中间断开连接”。
+func (c *Ctx) SendStream(step func(w io.Writer) bool) bool {
+	return c.ctx.Stream(step)
+}
+
+// SendReader 从 io.Reader 发送数据
+// size: 数据长度 (如果未知传 -1)
+// contentType: 显式指定类型，为空则尝试自动探测或使用 header。
+func (c *Ctx) SendReader(reader io.Reader, size int64, extraHeaders ...map[string]string) error {
+	c.ctx.Abort()
+	ct := c.GetHeader(HeaderContentType)
+	var headers map[string]string
+
+	if len(extraHeaders) > 0 {
+		headers = extraHeaders[0]
+	}
+
+	c.ctx.DataFromReader(c.StatusCode(), size, ct, reader, headers)
+	return nil
+}
+
+// SendSSEvent 将服务器发送事件写入正体流
+func (c *Ctx) SendSSEvent(name string, message any) error {
+	c.ctx.Abort()
+	c.ctx.SSEvent(name, message)
+	return nil
+}
+
+// Redirect 返回到特定位置的 HTTP 重定向
+func (c *Ctx) Redirect(loc string) error {
+	c.ctx.Abort()
+	c.ctx.Redirect(c.StatusCode(), loc)
+	return nil
 }
 
 func (c *Ctx) Status(code int) *Ctx {
@@ -330,11 +395,25 @@ func (c *Ctx) Get(key string, value ...any) any {
 		c.ctx.Set(key, value[0])
 		return value[0]
 	}
-
 	if v, ok := c.ctx.Get(key); ok {
 		return v
 	}
+	return c.ctx.Value(key)
+}
 
+func (c *Ctx) Deadline() (deadline time.Time, ok bool) {
+	return c.ctx.Deadline()
+}
+
+func (c *Ctx) Done() <-chan struct{} {
+	return c.ctx.Done()
+}
+
+func (c *Ctx) Err() error {
+	return c.ctx.Err()
+}
+
+func (c *Ctx) Value(key any) any {
 	return c.ctx.Value(key)
 }
 
